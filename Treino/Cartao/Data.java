ccb109b3bc0b99cb1447bd3b746843cc109f7b84
João Pedro <jpbrandes800@gmail.com>
2026-04-28 20:50:31 -0300

    Classe Data

---
+++ Treino/Cartao/Data.java
@@ -0,0 +1,40 @@
+public class Data {
+	private int dia;
+	private int mes;
+	private int ano;
+
+	public Data(int dia, int mes, int ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+
+	public int getDia() {
+		return this.dia;
+	}
+
+	public int getMes() {
+		return this.mes;
+	}
+
+	public int getAno() {
+		return this.ano;
+	}
+
+	public void setDia(int dia){
+	this.dia = dia;
+	}
+
+	public void setMes(int mes){
+	this.mes = mes;
+	}
+
+	public void setAno(int ano){
+	this.ano = ano;
+	}
+	
+	@Override
+	public String toString(){
+		return "Data: " + getDia() + "/" +  getMes() + "/" + getAno() + "/";
+		}
+}
