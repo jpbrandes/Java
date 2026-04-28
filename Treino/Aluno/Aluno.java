26f8b2bcf3a7f3b67abb35e6c67b1715082c2312
João Pedro <jpbrandes800@gmail.com>
2026-04-28 20:45:49 -0300

    Classe Aluno

---
+++ Treino/Aluno/Aluno.java
@@ -0,0 +1,22 @@
+public class Aluno extends Pessoa {
+    private String ra;
+
+    public Aluno(String nome, String telefone, Data nascimento, String ra) {
+        super(nome, telefone, nascimento);
+        this.ra = ra;
+    }
+
+    public Aluno(String nome, Data nascimento, String ra) {
+        super(nome, nascimento);
+        this.ra = ra;
+    }
+
+    public String getRa() { return this.ra; }
+
+    public void setRa(String ra) { this.ra = ra; }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nRA: " + ra;
+    }
+}
