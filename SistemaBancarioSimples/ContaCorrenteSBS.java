078dc999047ecb4a34f43270174960ee6c132891
jpbrandes <jpbrandes800@gmail.com>
2026-05-11 10:42:35 -0300

    Correções

--- SistemaBancarioSimples/ContaCorrenteSBS.java
+++ SistemaBancarioSimples/ContaCorrenteSBS.java
@@ -2,9 +2,9 @@ public class ContaCorrenteSBS {
     private String titular;
     private double saldo;
 
-    public ContaCorrenteSBS(String titular, double saldoInicial) {
+    public ContaCorrenteSBS(String titular, double saldo) {
         this.titular = titular;
-        this.saldo = saldoInicial;
+        this.saldo = saldo;
     }
 
 
@@ -16,12 +16,22 @@ public class ContaCorrenteSBS {
         return saldo;
     }
 
+    // Método para depositar e sacar dinheiro na conta
     public void depositar(double valor) {
         if (valor > 0) {
             saldo += valor;
-            System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso."); // Concatenação de string para exibir o valor depositado
         } else {
             System.out.println("Valor de depósito inválido.");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+            System.out.println("Saque de R$" + valor + " realizado com sucesso."); // Concatenação de string para exibir o valor sacado
+        } else {
+            System.out.println("Valor de saque inválido ou saldo insuficiente.");
         }
     }
 }
