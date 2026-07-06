87e3e3c40dad9f40a43010c801b1fcc740798096
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 13:35:34 -0300

    Create Square.java file

---
+++ Geometria/Square.java
@@ -0,0 +1,40 @@
+package Geometry;
+
+public class Square implements GeometricShape, ScalableShape {
+    double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double calculateArea() {
+        return side * side;
+    }
+
+    public double calculatePerimeter() {
+        return 4 * side;
+    }
+
+    public double getDecrease() {
+        return 0;
+    }
+
+    public double getIncrease() {
+        return 0;
+    }
+
+    public void setDecrease(double decrease) {
+        // Implementation for setting decrease
+    }
+
+    public void setIncrease(double increase) {
+        // Implementation for setting increase
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                '}';
+    }
+}
